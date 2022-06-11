af252068e3f0523b449b3348db91146fa66f3bf7
12Gustavo21 <gustavo927318@gmail.com>
2022-06-11 16:28:51 -0300

    Desafio-de-logica02

---
+++ script.js
@@ -0,0 +1,42 @@
+//Questão 01
+let idade = 17;
+if (idade >= 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
+
+//Questão 02
+let maiorDeIdade = 18;
+if (idade >= maiorDeIdade) {
+    console.log("Verdadeiro");
+} else {
+    console.log("Falso");
+}
+
+//Questão 03
+let aniversario = "Abril"
+if ((aniversario === "Janeiro") || (aniversario === "Dezembro")) {
+    console.log("Verdaeiro");
+} else {
+    console.log("Falso");
+}
+
+//Questão 04
+let nome = "Gustavo"
+let letraInicialNome = "G"
+if (letraInicialNome === "R") {
+    console.log("Veradeiro");
+} else {
+    console.log("Falso");
+}
+
+//Questão 06
+let sobreNome = "Almeida"
+let letraInicialSobrenome = "A";
+let numeroSobrenome = 7;
+if ((numeroSobrenome === 6) || (letraInicialSobrenome === "E")) {
+    console.log("Verdadeiro");
+} else {
+    console.log("Falso")
+}
